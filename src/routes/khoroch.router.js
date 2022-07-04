f2633cb08cbc0787058bda3e6ca9fc7d869b9907
moshiurr_ <mrahman9@upei.ca>
2022-07-03 21:24:36 -0300

    Khoroch creation done

--- src/routes/khoroch.router.js
+++ src/routes/khoroch.router.js
@@ -1,13 +1,41 @@
 const router = require('express').Router();
 const User = require('../models/user');
 const authMiddleware = require('../middlewares/auth');
+const ExpenseGroup = require('../models/expenseGroup');
+const Expense = require('../models/expense');
 
 //admin power for a expense group
 // can create a expense group, add user, delete user, name/rename group, block user from a group
 
 //TODO: route for creating a expense group
 router.post('/create', authMiddleware, async (req, res)=>{
+      const {members, groupName} = req.body;
 
+      try {
+            //checking if all the members are valid or not
+            let areAllMembersActive = await ExpenseGroup.validateMembers(members);
+            if(!areAllMembersActive) throw new Error("One of the member is not valid");
+
+            // adding the user itself inside the members array
+            members.push(req.user._id);
+            
+            const newExpenseGroupData = { admin: req.user._id, members, groupName};
+
+            let expenseGroup = new ExpenseGroup(newExpenseGroupData);
+
+            await expenseGroup.save();
+
+            return res.send({
+                  success: true,
+                  message: "Expense group saved successfully"
+            })
+            
+      } catch (error) {
+            return res.status(500).send({
+                  success: false,
+                  message: error.message
+            })
+      }
 });
 
 //TODO: route getting a expense group information
